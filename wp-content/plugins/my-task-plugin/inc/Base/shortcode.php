8a3c17e7ba227652a60b2cf0ec6873890865b64c
chyousuf <ch.yousuf.javaid@gmail.com>
2022-10-06 04:22:45 +0500

    spread sheet shortcode

--- wp-content/plugins/my-task-plugin/inc/Base/shortcode.php
+++ wp-content/plugins/my-task-plugin/inc/Base/shortcode.php
@@ -1,5 +1,5 @@
 <?php
-function google_spread_sheet_api($spread_sheet_Id, $spread_sheet_name)
+function google_spread_sheet_api($spread_sheet_Id, $spread_sheet_name, $spread_sheet_Range)
 {
     // configure the Google Client
     $client = new \Google_Client();
@@ -17,56 +17,78 @@ function google_spread_sheet_api($spread_sheet_Id, $spread_sheet_name)
     $spreadsheetId = $spread_sheet_Id;
     $spreadsheet = $service->spreadsheets->get($spreadsheetId);
     // get all the rows of a sheet
-    $range = $spread_sheet_name; // here we use the name of the Sheet to get all the rows
+    if (!empty($spread_sheet_Range)) {
+        $range = $spread_sheet_name . '!' . $spread_sheet_Range;
+    } else {
+        $range = $spread_sheet_name; // here we use the name of the Sheet to get all the rows
+    }
     try {
         $response = $service->spreadsheets_values->get($spreadsheetId, $range);
         $values = $response->getValues();
-        foreach ($values as $key => $value) {
-
-            if (in_array('Date', $value)) {
-                var_dump($key);
+        echo '<table>';
+        $i = 0;
+        foreach ($values as $key => $valuez) {
+            foreach ($valuez as $key => $value) {
+                if ($i == 0) {
+                    echo  '<tr><th>' . $value . '</th></tr>';
+                } else {
+                    echo '<tr><td>' . $value . '</td></tr>';
+                }
+                $i++;
             }
         }
+        echo '</table>';
     }
     //catch exception
     catch (Exception $e) {
-        echo 'Message: ' . $e->getmessage();
+        echo 'Message: Unable to parse range';
     }
 }
-add_shortcode('cm', 'shortcode_sheet_callback_function');
-function shortcode_sheet_callback_function($atts)
-{
-    // Spread Sheet ID
-    $options = get_option('my_task_sheet_field_plugin_options');
-    if (isset($atts['spread_sheet_filed_id']) && !empty($atts['spread_sheet_filed_id'])) {
-        $atts['spread_sheet_filed_id'] = (int)$atts['spread_sheet_filed_id'];
-        if ($atts['spread_sheet_filed_id'] > 0) {
-            $id = $atts['spread_sheet_filed_id'] - 1;
-        } else {
-            $id = 0;
-        }
-        $sheet_field = $options['sheet_field'][$id];
-        if (!empty($sheet_field)) {
-            $spread_sheet_Id = $sheet_field;
+//ShortCode
+if (!is_admin()) {
+    add_shortcode('cm', 'shortcode_sheet_callback_function');
+    function shortcode_sheet_callback_function($atts)
+    {
+
+        // Spread Sheet ID
+        $options = get_option('my_task_sheet_field_plugin_options');
+        if (isset($atts['spread_sheet_filed_id']) && !empty($atts['spread_sheet_filed_id'])) {
+            $atts['spread_sheet_filed_id'] = (int)$atts['spread_sheet_filed_id'];
+            if ($atts['spread_sheet_filed_id'] > 0) {
+                $id = $atts['spread_sheet_filed_id'] - 1;
+            } else {
+                $id = 0;
+            }
+            $sheet_field = $options['sheet_field'][$id];
+            if (!empty($sheet_field)) {
+                $spread_sheet_Id = $sheet_field;
+            } else {
+                $spread_sheet_Id =  $options['sheet_field'][0];
+            }
         } else {
             $spread_sheet_Id =  $options['sheet_field'][0];
         }
-    } else {
-        $spread_sheet_Id =  $options['sheet_field'][0];
-    }
-    // Spread Sheet ID
+        // Spread Sheet ID
 
-    // Spread Sheet Name
-    if (isset($atts['spread_sheet_name']) && !empty($atts['spread_sheet_name'])) {
-        $spread_sheet_name = $atts['spread_sheet_name'];
-    } else {
-        $spread_sheet_name = 'Sheet1';
+        // Spread Sheet Name
+        if (isset($atts['spread_sheet_name']) && !empty($atts['spread_sheet_name'])) {
+            $spread_sheet_name = $atts['spread_sheet_name'];
+        } else {
+            $spread_sheet_name = 'Sheet1';
+        }
+        // Spread Sheet Name
+        // Spread Sheet Range
+        if (isset($atts['spread_sheet_range']) && !empty($atts['spread_sheet_range'])) {
+            $spread_sheet_Range = $atts['spread_sheet_range'];
+        }
+
+        // Spread Sheet Range
+        extract(shortcode_atts(array(
+            'spread_sheet_Id' => $spread_sheet_Id,
+            'spread_sheet_name' => $spread_sheet_name,
+            'sheet_Range' => $spread_sheet_Range
+        ), $atts));
+
+        google_spread_sheet_api($spread_sheet_Id, $spread_sheet_name, $spread_sheet_Range);
     }
-    // Spread Sheet Name
-    extract(shortcode_atts(array(
-        'spread_sheet_Id' => $spread_sheet_Id,
-        'spread_sheet_name' => $spread_sheet_name,
-    ), $atts));
-    // var_dump($spread_sheet_name);
-    google_spread_sheet_api($spread_sheet_Id, $spread_sheet_name);
 }
